7132d03db8b96b88b71fa1fe9cd63df1289edb3c
anicodesit <ani.kulka@gmail.com>
2021-05-04 18:50:31 -0400

    Sale Controller created

--- ResaleDashboardMVC/Controllers/SaleController.cs
+++ ResaleDashboardMVC/Controllers/SaleController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Models.SaleModels;
+using ResaleDashboardMVC.Data;
+using ResaleDashboardMVC.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,76 @@ namespace ResaleDashboardMVC.Controllers
 {
     public class SaleController : Controller
     {
-        // GET: SAle
+        SaleServices saleServ = new SaleServices();
+        // GET: Sale
+        [HttpGet]
+        //[ValidateAntiForgeryToken]
         public ActionResult Index()
         {
+            List<Sale> saleList = saleServ.SaleIndex();
+            return View(saleList);
+        }
+        // Get: Sale/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+        //Post: Sale/Create
+        [HttpPost]
+        public ActionResult Create(SaleCreate model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            if (saleServ.SaleCreate(model))
+            {
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Your platform could not be added. Please try again. ");
+            return View(model);
+        }
+        // Get: Sale/Edit{id}
+        public ActionResult Edit(int id)
+        {
+
+            SaleEdit sale = saleServ.SaleFind(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+        //POST: Sale/Edit{id}
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Edit(Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(sale);
+        }
+
+        //Get: Sale/Delete/{id}
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var model = saleServ.SaleFind(id);
+            return View(model);
+        }
+
+        //Post: Sale/Delete/{id}
+        [HttpPost]
+        [ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public ActionResult DeleteSale(int id)
+        {
+            saleServ.SaleDelete(id);
+
+            //Feeling cute, might delete this later
+            TempData["SaveResult"] = "Your sale was deleted ";
+
+            return RedirectToAction("Index");
         }
     }
 }
