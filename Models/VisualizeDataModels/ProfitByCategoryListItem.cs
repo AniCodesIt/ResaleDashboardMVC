391ca93af82f2402b26cc24932c23e66c1f3f7bd
anicodesit <ani.kulka@gmail.com>
2021-05-17 20:15:53 -0400

    Created model and service for VisualizeData, wrote helper method to find categories

---
+++ Models/VisualizeDataModels/ProfitByCategoryListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResaleDashboardMVC.Models
+{
+    public class ProfitByCategoryListItem
+    {
+        public string Category { get; set; }       
+        public decimal Profit { get; set; }
+    }
+}
---
+++ Services/VisualizeDataService.cs
@@ -0,0 +1,104 @@
+﻿
+using ResaleDashboardMVC.Data;
+using ResaleDashboardMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ResaleDashboardMVC.Services
+{
+    public class VisualizeDataService
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+       
+        
+        public decimal profitFinder(SaleListItem saleEntity)
+        {
+            var invEntity = _db.Inventories.SingleOrDefault(e => e.InvID == saleEntity.InvID);
+            var platEntity = _db.Platforms.SingleOrDefault(e => e.PlatformID == saleEntity.PlatformID);
+            //profit = sales price - fees - COG
+            decimal profit = saleEntity.SalePrice - (saleEntity.SalePrice * platEntity.Fees) - invEntity.COG;
+            return profit;
+        }
+        //keep
+        public decimal profitFinder(Sale saleEntity)
+        {
+            var invEntity = _db.Inventories.SingleOrDefault(e => e.InvID == saleEntity.InvID);
+            var platEntity = _db.Platforms.SingleOrDefault(e => e.PlatformID == saleEntity.PlatformID);
+            //profit = sales price - fees - COG
+            decimal profit = saleEntity.SalePrice - (saleEntity.SalePrice * platEntity.Fees) - invEntity.COG;
+            return profit;
+        }
+        //keep
+        public string categoryFinder(SaleListItem saleEntity)
+        {
+            var invEntity = _db.Inventories.SingleOrDefault(e => e.InvID == saleEntity.InvID);        
+            string category = invEntity.Category;
+            return category;
+        }
+        //keep
+        public List<PlatformSalesListItem> VisualizeSales()
+        {
+            //public List<SaleListItem> SaleIndex()
+            int index = 0;
+            decimal newTotal = 0;
+            List<SaleListItem> saleVisuals = SaleIndex();
+            List<PlatformSalesListItem> platformVisuals = new List<PlatformSalesListItem>();
+            foreach(SaleListItem sale in saleVisuals)
+            {
+                //index = platformVisuals.Find(p => p.PlatformID == sale.PlatformID)
+                    index = platformVisuals.FindIndex(p => p.PlatformID == sale.PlatformID);
+                if (index == 0)
+                {
+                    PlatformSalesListItem mp = new PlatformSalesListItem();
+                    mp.PlatformID = sale.PlatformID;
+                    mp.SalePrice = sale.SalePrice;
+                    platformVisuals.Add(mp);
+                }
+                else
+                {
+                    //We know what row this platform is on.
+                    //get the SalePrice from that row.
+                    //add the saleprice from this sale to the amount we found above.
+                    //update the row in platformvisuals with the new sales total.
+                    newTotal = platformVisuals[index].SalePrice + sale.SalePrice;
+                    platformVisuals[index].SalePrice = newTotal;
+
+                }
+            }
+                return platformVisuals;
+        }
+        //Profit by Platform
+        public List<ProfitByCategoryListItem> VisualizeProfitByCategory()
+        {            
+            int index = 0;
+            decimal newProfit = 0;
+            List<SaleListItem> saleList = SaleIndex();
+            List<ProfitByCategoryListItem> profitByCategoryVisuals = new List<ProfitByCategoryListItem>();
+            foreach (SaleListItem sale in saleList)
+            {                
+                index = profitByCategoryVisuals.FindIndex(p => p.Category == categoryFinder(sale));
+                if (index == 0) //If this category is not in the output list yet
+                {
+                    ProfitByCategoryListItem mp = new ProfitByCategoryListItem();
+                    mp.Category = categoryFinder(sale);
+                    mp.Profit = sale.Profit;
+                    profitByCategoryVisuals.Add(mp);
+                }
+                else
+                {
+                    //This category was already in the list, now add this profit to the listitem
+                    newProfit = profitByCategoryVisuals[index].Profit + sale.Profit;
+                    profitByCategoryVisuals[index].Profit = newProfit;
+                }
+            }
+            return profitByCategoryVisuals;
+        }
+
+    }
+
+}
+
